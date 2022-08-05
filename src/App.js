c8692db95a2761ccd92930d44fc580508fa16c8c
ilhomjonSolijonov <ilhomjon206solijonov@gmail.com>
2022-08-05 14:44:04 +0500

    add the Navbar section to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-
+import {Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 
 function App() {
   return (
-   <>
-
-    
-   </>
+   <Box width="400px" sx={{width: { xl: '1448px' }}} m='auto'>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/exercise/:id' element={<ExerciseDetail/>} />
+    </Routes>
+    <Footer />
+   </Box>
   );
 }
 
