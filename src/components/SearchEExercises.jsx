8cf87d21ba9639ca2c30446ed24bc402f8023005
ilhomjonSolijonov <ilhomjon206solijonov@gmail.com>
2022-08-05 16:21:40 +0500

    i am compleate the SearchExercises.jsx and anymore inam start fetchData

--- src/components/SearchEExercises.jsx
+++ src/components/SearchEExercises.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { Box, Button, Stack, TextField, Typography} from '@mui/material'
+
 
 const SearchEExercises = () => {
+
+  const [search, setSearch] = useState('')
+
+  const handleSearch = async () =>{
+    if(search){
+      // const exerciseData = await fetchDate();
+    }
+  }
+
   return (
-    <div>SearchEExercises</div>
+    <Stack alignItems="center" mt='37px' justufyContent="center" p="20px">
+      <Typography
+        fontWeight='700'
+        mb="50px"
+        textAlign='center'
+        sx={{
+          fontSize: {lg: '44px', xs: '30px'}
+        }}
+      >
+        Somthinf Exercises You <br/> Should Know
+      </Typography>
+      <Box position="relative" mb='72px'>
+        <TextField       
+          sx={{
+            input: {
+              fontWeight: '700', 
+              border: 'none', 
+              borderRadius:'4px'
+            }, 
+            width:{
+              lg: '800px', xs: '350px'
+            },
+            backgroundColor: '#fff', 
+            borderRadius: '40px', 
+          }}
+          height='76px'
+          value={search}
+          onChange={(e) => setSearch=e.target.value.toLowerCase()}
+          placeholder='Search Exercises'
+        />
+        <Button 
+          className="search-btn"
+          sx={{
+            bgcolor: '#ff2625', 
+            color: '#fff',
+            width: {
+              lg: '175px', 
+              xs: '80px'
+            }, 
+            fontSize: {
+              lg: '20pxpx', 
+              xs: '14px'
+            }, 
+            position: 'absolute',
+            height: '57px', 
+            right: '0'
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
