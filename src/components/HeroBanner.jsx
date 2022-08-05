2aa0194c978c84797aaaeaa9933faecc1c76081b
ilhomjonSolijonov <ilhomjon206solijonov@gmail.com>
2022-08-05 15:33:17 +0500

    add HeroBanner and write a code so that was a greate

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -16,17 +16,38 @@ const HeroBanner = () => {
         sx={{
           fontSize: {lg: '44px', xs: '40px'}
         }}
+        mb="23px" mt="30px"
       >
         Sweat, Smile <br/> and Repeat
       </Typography>
       <Typography 
         fontSize='22px'
         lineHeight='35px'
-        mb={3}
+        mb={4}
       >
         Check out the most Effective exercises
       </Typography>
-      <Button variant="contained" color="error">Explore Exercises</Button>
+      <Button 
+        variant="contained"
+        color="error"
+        sx={{
+          backgroundColor: '#ff2625', 
+          padding:'10px'
+        }}
+      >
+          Explore Exercises
+      </Button>
+      <Typography
+        fontWeight={600}
+        color='#ff2625'
+        fontSize='200px'
+        sx={{
+          opacity: 0.1, 
+          display: {lg: 'block', xs: 'none'}
+        }}
+      >
+        Exercises
+      </Typography>
       <img src={HeroBannerImage} alt='banner' className='hero-banner-img' />
     </Box>
   )
